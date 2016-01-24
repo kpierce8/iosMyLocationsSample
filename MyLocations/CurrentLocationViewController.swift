853ce2d40b96d315d64877d653fe7cc3237d885c
Ken Pierce <kpierce8@gmail.com>
2016-01-24 10:26:29 -0800

    up to page 70 in tutorial

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,250 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by UberDominator on 11/18/15.
+//  Copyright © 2015 Frantic Goose Applications. All rights reserved.
+//
+import CoreLocation
+import UIKit
+
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
+    var location: CLLocation?
+    var updatingLocation = false
+    var lastLocationError: NSError?
+    let geocoder = CLGeocoder()
+    var placemark: CLPlacemark?
+    var performingReverseGeocoding = false
+    var lastGeocodingError: NSError?
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    @IBAction func getLocation() {
+        let authStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == .NotDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .Denied || authStatus == .Restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        if updatingLocation {
+            stopLocationManager()
+        } else {
+            location = nil
+            lastLocationError = nil
+            placemark = nil
+            lastGeocodingError = nil
+            startLocationManager()
+        }
+        updateLabels()
+        configureGetButton()
+    }
+    
+    
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in Settings", preferredStyle: .Alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+    
+        alert.addAction(okAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func configureGetButton(){
+        if updatingLocation {
+            getButton.setTitle("Stop", forState: .Normal)
+        } else {
+            getButton.setTitle("Get my location", forState: .Normal)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLabels()
+        configureGetButton()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    // MARK: - CCLocationManagerDelegate
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("didFailWithError \(error)")
+        
+        if error.code == CLError.LocationUnknown.rawValue {
+            return
+        }
+        
+        lastLocationError = error
+        
+        stopLocationManager()
+        updateLabels()
+        configureGetButton()
+    }
+    
+    
+    func startLocationManager() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            updatingLocation = true
+        }
+    }
+    
+    func stopLocationManager(){
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+       
+        if newLocation.timestamp.timeIntervalSinceNow < -5 {
+            return
+        }
+        
+        if newLocation.horizontalAccuracy < 0 {
+            return
+        }
+        
+        var distance = CLLocationDistance(DBL_MAX)
+        if let location = location {
+            distance = newLocation.distanceFromLocation(location)
+        }
+        
+        
+        
+        if location == nil || location!.horizontalAccuracy > newLocation.horizontalAccuracy {
+             lastLocationError = nil
+        location = newLocation
+        updateLabels()
+            
+            
+            if newLocation.horizontalAccuracy <= locationManager.desiredAccuracy {
+                print("*** We're done!")
+                stopLocationManager()
+                configureGetButton()
+            }
+   
+            if !performingReverseGeocoding {
+                print("going to geocode")
+                
+                performingReverseGeocoding = true
+                
+                geocoder.reverseGeocodeLocation(newLocation, completionHandler: {
+                     placemarks, error in
+                    
+                    print("*** Found placemarks: \(placemarks), error: \(error)")
+                    
+                    self.lastGeocodingError = error
+                    if error == nil, let p = placemarks where !p.isEmpty {
+                        self.placemark = p.last!
+                    } else {
+                        self.placemark = nil
+                    }
+
+                    self.performingReverseGeocoding = false
+                    self.updateLabels()
+                    
+                    })
+            }
+           
+        }
+    }
+    
+    func stringFromPlacemark(placemark: CLPlacemark) -> String {
+        var line1 = ""
+        if let s = placemark.subThoroughfare {
+            line1 += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            line1 += s + " "
+        }
+        
+        var line2 = ""
+        if let s = placemark.locality {
+            line2 += s + " "
+        }
+        if let s = placemark.administrativeArea {
+            line2 += s + " "
+        }
+        if let s = placemark.postalCode {
+            line2 += s
+        }
+        
+        return line1 + "\n" + line2
+    }
+    
+    func updateLabels(){
+        
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            
+            tagButton.hidden = false
+            messageLabel.text = ""
+            
+            
+            if let placemark = placemark {
+                addressLabel.text = stringFromPlacemark(placemark)
+            } else if performingReverseGeocoding {
+                addressLabel.text = "Searching for address ..."
+            } else if lastGeocodingError != nil {
+                addressLabel.text = "Error finding address"
+            } else {
+                addressLabel.text = "No address found"
+            }
+            
+            
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.hidden = true
+            
+            let statusMessage: String
+            if let error = lastLocationError {
+                if error.domain == kCLErrorDomain &&
+                    error.code == CLError.Denied.rawValue {
+                        statusMessage = "Location Services Disabled"
+                } else {
+                    statusMessage = "Error getting location"
+                }
+            } else if !CLLocationManager.locationServicesEnabled() {
+                statusMessage = "Location Services Disabled"
+
+            } else if updatingLocation {
+                statusMessage = "Searching ..."
+            } else {
+                statusMessage = "Tap 'Get My Location' to STart"
+            }
+            messageLabel.text = statusMessage
+        }
+    }
+    
+    
+}
+
